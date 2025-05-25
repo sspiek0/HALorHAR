878108c64dc68b59898c2316229daf1a5a6e68cb
katkasoft <katkaleks@gmail.com>
2025-05-25 15:20:20 +0300

    malloc

---
+++ malloc.cpp
@@ -0,0 +1,12 @@
+﻿#include <windows.h>
+#include <cmath>
+#include <cstdlib>
+
+int main() {
+    int i = 0;
+    while (true) {
+        double result = std::sin(i) * std::cos(i);
+        malloc(1024 * 1024);
+    }
+    return 0;
+}
